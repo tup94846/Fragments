ae6e3f8d4d003c3ed0ebdbf699914a6fcf0834cc
tup94846 <150381476+tup94846@users.noreply.github.com>
2025-03-13 11:32:11 -0400

    Selecting color in fragments

--- app/src/main/java/edu/temple/fragments/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragments/ColorFragment.kt
@@ -1,10 +1,13 @@
 package edu.temple.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -15,12 +18,37 @@ class ColorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false).apply {
-            findViewById<Spinner>(R.id.spinner).apply {
-                adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, arrayOf("Blue", "Red", "Black", "White", "Magenta"))
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        this.view?.apply {
+            findViewById<Spinner>(R.id.spinner).apply {
+                adapter = ArrayAdapter(
+                    context,
+                    android.R.layout.simple_spinner_item,
+                    arrayOf("Blue", "Red", "Black", "White", "Magenta")
+                )
+
+                onItemSelectedListener = object : OnItemSelectedListener {
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        p0?.run {
+                            this@ColorFragment.view?.setBackgroundColor(
+                                Color.parseColor(
+                                    getItemAtPosition(p2).toString()
+                                )
+                            )
+                        }
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+
+                    }
+                }
             }
         }
     }
-
 }
+
